42fe0215323ec0c46ca90f3414f61dcde754262a
priyanka7569 <kota.priyanka@mphasis.com>
2023-12-08 19:25:27 +0530

    Practise project4 modified

---
+++ OOP Application/Subjects.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractiseProject4
+{
+    internal class Subjects
+    {
+        public string SubjectName;
+        public char SubjectCode;
+        public string Teacher;
+    }
+}
